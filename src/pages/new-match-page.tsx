7aeabd99b8a5fe7174ceaee571ad150bdade1c0f
Behnam Salehi <behnam.dev@outlook.com>
2026-02-10 12:15:12 +0330

    1

--- src/pages/new-match-page.tsx
+++ src/pages/new-match-page.tsx
@@ -1,3 +1,130 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Slider } from "@/components/ui/slider";
+
+const formSchema = z.object({
+  rounds: z.number().min(1, "").max(30, ""),
+});
+
 export function NewMatchPage() {
-  return <div>NewMatchPage</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      rounds: 10,
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {}
+
+  return (
+    <div>
+      <div>NewMatchPage</div>
+
+      <Card className="w-full sm:max-w-md">
+        <CardHeader>
+          <CardTitle>Bug Report</CardTitle>
+          <CardDescription>
+            Help us improve by reporting bugs you encounter.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller
+                name="rounds"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="rounds">Rounds</FieldLabel>
+                    <Slider
+                      {...field}
+                      defaultValue={[15]}
+                      min={1}
+                      max={30}
+                      step={1}
+                      className="mx-auto w-full max-w-xs"
+                    />
+                  </Field>
+                )}
+              />
+              <Controller
+                name="title"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor="form-rhf-demo-title">
+                      Bug Title
+                    </FieldLabel>
+                    <Input
+                      {...field}
+                      id="form-rhf-demo-title"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Login button not working on mobile"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Field orientation="horizontal">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => form.reset()}
+            >
+              Reset
+            </Button>
+            <Button type="submit" form="form-rhf-demo">
+              Submit
+            </Button>
+          </Field>
+        </CardFooter>
+      </Card>
+
+      <div>
+        <FieldSet>
+          <FieldLegend>Profile</FieldLegend>
+          <FieldDescription>
+            This appears on invoices and emails.
+          </FieldDescription>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="name">Full name</FieldLabel>
+              <Input id="name" autoComplete="off" placeholder="Evil Rabbit" />
+              <FieldDescription>
+                This appears on invoices and emails.
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </FieldSet>
+      </div>
+    </div>
+  );
 }
